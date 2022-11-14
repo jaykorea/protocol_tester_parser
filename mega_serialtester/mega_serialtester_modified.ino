e65fec86bc59f5364321d016ec6b0aa4beaa7841
jay <jhjo1997@naver.com>
2022-11-14 11:22:20 +0900

    modified general protocol2

--- mega_serialtester/mega_serialtester_modified.ino
+++ mega_serialtester/mega_serialtester_modified.ino
@@ -1,3 +1,4 @@
+String hash[5] = {"0000010000020000>", "0000000000000000>", "0000000000010011>", "0004000000000000>", "0001000100020001>"};
 void setup() {
 Serial.begin(115200);
 Serial1.begin(115200);
@@ -18,22 +19,27 @@ void loop() {
   data[9]= random(10,99);
   sprintf(send_data, "%d%d%d%d%d%d%d%d%d%c",data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],'>');
   String send_string="011121314151617121516>";
-  char hash_02[] = "0000000000000000>";
-  char hash_05[] = "0001000100020011>";
+  char hash_char[] = "0000000000000000>";
+
   if (Serial1.available() > 0 )
   {
+    static int ct= 0;
     String cmdString = "at+qd1?";
     String recString = Serial1.readStringUntil('\n');
     int index = recString.indexOf('\r');
     String parsedString = recString.substring(0, index);
-    Serial.println(send_data);
+    //Serial.println(send_data);
+    if (ct > 4) {
+        ct = 0;
+      }
     if (parsedString.equals(cmdString))
     {
       //Serial2.println(gyro_buf);
-      Serial1.print(hash_05);
+      Serial1.print(hash_char);
+      ct++;
     }
   }
-  delay(15);
+  delay(1);
 }
 
 //if(Serial.available()){
